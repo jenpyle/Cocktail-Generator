76dfd80a6fba9f43c181ceb046c474a5ff3275b5
jenpyle <jennypyle440@gmail.com>
2021-02-02 15:00:31 -0700

    Update script.js and styles.css to format output on webpage

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,8 @@ let cocktailList = [
     name: "Hunter's Moon",
     category: 'Cocktail',
     glass: 'Balloon Glass',
-    ingredients: [
-      'Vermouth',
-      'Maraschino Cherry',
-      'Sugar Syrup',
-      'Lemonade',
-      'Blackberries',
-    ],
+    // prettier-ignore
+    ingredients: ['Vermouth','Maraschino Cherry','Sugar Syrup','Lemonade','Blackberries',],
     instructions:
       'Put the Bombay Sapphire, Martini Bianco, sugar syrup & blackberries in a cocktail shaker with lots of ice and shake vigorously before pouring into a balloon glass, topping up with lemonade and garnishing with a wedge of orange.',
   },
@@ -24,8 +19,8 @@ let cocktailList = [
     name: 'Mulled Wine',
     category: 'Punch / Party Drink',
     glass: 'Collins Glass',
-    ingredients: [
-      'Water',
+    // prettier-ignore
+    ingredients: ['Water',
       'Sugar',
       'Cloves',
       'Cinnamon',
@@ -46,18 +41,32 @@ let cocktailList = [
   },
 ];
 
+let container = '<div class="container">';
+document.write(container);
 for (let i = 0; cocktailList[i]; i++) {
-  let text = 'Cocktail name: ' + cocktailList[i].name + ' Category: ' + cocktailList[i].category + ' ';
-  document.write(text+ '\n' + 'Ingredients: ');
-  
+  let text =
+    '<h3>Cocktail name:</h3> ' +
+    cocktailList[i].name +
+    '<br /> <h3>Category: </h3>' +
+    cocktailList[i].category +
+    ' <br />';
+  document.write(text + '<h3>Ingredients:</h3> ');
+
   let drinkIngredients = '';
 
   for (let j = 0; j < cocktailList[i].ingredients.length; j++) {
-    if(j < cocktailList[i].ingredients.length-1)/*check if it is NOT the last word, only have commas if another word follows*/
-      drinkIngredients = drinkIngredients + cocktailList[i].ingredients[j].trim() + ', ';
-    else
-      drinkIngredients = drinkIngredients + 'and ' + cocktailList[i].ingredients[j].trim() + '.';
+    if (j < cocktailList[i].ingredients.length - 1) {
+      /*check if it is NOT the last word, only have commas if another word follows*/
+      drinkIngredients =
+        drinkIngredients + cocktailList[i].ingredients[j].trim() + ', ';
+    } else {
+      drinkIngredients =
+        drinkIngredients +
+        'and ' +
+        cocktailList[i].ingredients[j].trim() +
+        '.<br /><br />';
+    }
   }
-  document.write(drinkIngredients + "\n");
+  document.write(drinkIngredients);
 }
-
+document.write('</div>');
