09a1c579161c13ddcb0c0c987828beae5ae2fe23
jenpyle <jennypyle440@gmail.com>
2021-02-11 16:21:43 -0700

    Implement Ajax for 3 promise functions -- Has errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
+//what parameters can I use in the promises?
+//how to display image on page?
+
 let cocktailRepository = (function () {
   let cocktailList = [];
-  let apiUrl =
-    'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail';
+  let apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
 
   function add(drink) {
-    if (typeof drink === 'object' && 'strDrink' in drink) {
+    //if (typeof drink === 'object' && 'strDrink' in drink) { ---Why doesn't this work?
+    if (typeof drink === 'object') {
       cocktailList.push(drink);
     } else {
       console.log('Drink object is incorrect');
@@ -15,11 +18,29 @@ let cocktailRepository = (function () {
     return cocktailList;
   }
 
+  function loadList() {
+    return fetch(apiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        json.drinks.forEach(function (item) {
+          let drinks = {
+            category: item.strCategory,
+          };
+          add(drinks);
+        });
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
+
   function addListItem(drink) {
     let element = document.querySelector('.cocktail-list');
     let listItem = document.createElement('li');
     let button = document.createElement('button');
-    button.innerText = drink.name;
+    button.innerText = drink.category;
     button.classList.add('drink-info-button');
     button.addEventListener('click', function () {
       showDetails(drink);
@@ -29,45 +50,67 @@ let cocktailRepository = (function () {
     showDetails(drink); //call new function inside addListItem() after button is appended to DOM
   }
 
-  function showDetails(drink) {
-    loadDetails(drink).then(function () {
-      console.log(drink);
-    });
-  }
-
-  // function showDetails(drink) {
-  //   //function that’s dedicated to adding the event listener to the newly created button
-  //   console.log(drink.name);
-  // }
-
-  function loadList() {
-    return fetch(apiUrl)
+  function loadDetails(item) {
+    // function loadDetails(drink) { -- why doesn't it work when i change item to drink
+    let selectedCategory = item.category;
+    selectedCategory = selectedCategory.replace(/\s+/g, '_');
+    let categoryUrl =
+      'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=' +
+      selectedCategory;
+    return fetch(categoryUrl)
       .then(function (response) {
-        console.log(response);
         return response.json();
       })
       .then(function (json) {
-        console.log(json);
-        console.log(json.results);
-        json.results.forEach(function (item) {
-          let drinks = {
-            name: item.strDrink,
-            // detailsUrl: item.url
-          };
-          add(drinks);
+        json.drinks.forEach(function (details) {
+          item.name = details.strDrink;
+          item.imageUrl = details.strDrinkThumb + '/preview';
         });
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
+  //! Need help assigning names to multiple drinks?
+  function loadMoreDetails(item) {
+    let selectedDrink = item.name;
+    selectedDrink = selectedDrink.replace(/\s+/g, '_');
+    let drinkUrl =
+      'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' +
+      selectedDrink;
+    return fetch(drinkUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (details) {
+        item.glass = details.strGlass;
+        console.log('--' + details.strGlass); //Why undefined?
+        item.instructions = details.strInstructions;
+        // item.ingredient = details.strIngredient1; add for-loop to iterate thru object?
+        item.isAlcoholic = details.strAlcoholic;
+        // item.measure = details.strMeasure1; add for-loop to iterate thru object?
       })
       .catch(function (e) {
         console.error(e);
       });
   }
 
+  function showDetails(drink) {
+    //function that’s dedicated to adding the event listener to the newly created button
+    loadDetails(drink).then(function () {
+      loadMoreDetails(drink);
+    });
+    console.log(drink);
+  }
+
   return {
     add: add,
     getAll: getAll,
-    addListItem: addListItem,
-    showDetails: showDetails,
     loadList: loadList,
+    addListItem: addListItem,
+    loadDetails: loadDetails,
+    loadMoreDetails: loadMoreDetails,
+    showDetails: showDetails,
   };
 })();
 
