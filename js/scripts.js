c621964b46c608973cdd8cb807de51eb87b5355b
jenpyle <jennypyle440@gmail.com>
2021-02-12 18:07:50 -0700

    Remove loader, Make drink items into buttons, add new function addDrinkListItem()

--- js/scripts.js
+++ js/scripts.js
@@ -6,64 +6,75 @@ let cocktailRepository = (function () {
     cocktailCategories.push(category);
   }
 
-  function getAll() {
-    return cocktailCategories;
-  }
-
-  function loadList() {
-    showLoadingIcon();
+  function loadCategoryList() {
     return fetch(apiUrl) // fetch returns a Promise
       .then(function (response) {
+        //same thing as last 2 lines of fetchDrinksByCategory function
         return response.json();
       })
       .then(function (json) {
-        hideLoadingIcon();
-        console.log(json, '!!drinks'); // check the Api response (json)
+        console.log(json, '!!drinks'); // check the Api response (json) to get the name 'drinks'
         json.drinks.forEach(function (item) {
           // this anonymous function is the callback of forEach function, item is the parameter, item is the element of the json.drinks array which is an object
           add(item.strCategory);
+          console.log('--- ' + cocktailCategories);
         });
       })
       .catch(function (error) {
-        hideLoadingIcon();
         console.error(error);
       });
   }
 
-  function addListItem(category) {
-    let element = document.querySelector('.cocktail-categories');
-    let listItem = document.createElement('li');
-    let button = document.createElement('button');
-    button.innerText = category;
-    button.classList.add('drink-info-button');
+  function getAll() {
+    return cocktailCategories;
+  }
 
-    button.addEventListener('click', function () {
+  function addCategoryListItem(category) {
+    let element = document.querySelector('.cocktail-categories');
+    let categoryListItem = document.createElement('li');
+    let categoryButton = document.createElement('button');
+    categoryButton.innerText = category;
+    categoryButton.classList.add('drink-info-button');
+
+    categoryButton.addEventListener('click', function () {
       fetchDrinksByCategory(category).then((drinks) => {
-        document.querySelector('.drinks').innerHTML = ''; // clear all the drinks, otherwise it would keep appending the drinks of the clicked category at the end
+        document.querySelector('.cocktail-drinks__buttons').innerHTML = ''; // clear all the drinks, otherwise it would keep appending the drinks of the clicked category at the end
         drinks.forEach((drink) => {
           // you can inspect api response under Network tab > XHR > Response
-          let drinkItem = document.createElement('div');
-          drinkItem.innerText = drink.strDrink;
-          document.querySelector('.drinks').appendChild(drinkItem);
+          // let drinkItem = document.createElement('li');
+          // let drinkButton = document.createElement('button');
+          // let element2 = document.querySelector('.cocktail-drinks__buttons');
+          // drinkItem.innerText = drink.strDrink;
+          // drinkItem.appendChild(drinkButton); //append button to list item
+          // element2.appendChild(drinkItem);
+          addDrinkListItem(drink);
         });
       });
     }); //Event Listener
-    listItem.appendChild(button); //append button to list item
-    element.appendChild(listItem); //append list item to parent
+    categoryListItem.appendChild(categoryButton); //append button to list item
+    element.appendChild(categoryListItem); //append list item to parent
     // showDetails(category); //call new function inside addListItem() after button is appended to DOM
   }
 
   function fetchDrinksByCategory(category) {
+    console.log('HERE');
     return fetch(
       `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`
     )
-      .then((response) => response.json())
+      .then((response) => response.json()) //what do these do?
       .then((json) => json.drinks);
   }
 
-  //! Need help assigning names to multiple drinks?
+  function addDrinkListItem(drink) {
+    let drinkItem = document.createElement('li');
+    let drinkButton = document.createElement('button');
+    let element2 = document.querySelector('.cocktail-drinks__buttons');
+    drinkItem.innerText = drink.strDrink;
+    drinkItem.appendChild(drinkButton); //append button to list item
+    element2.appendChild(drinkItem);
+  }
+
   function loadMoreDetails(item) {
-    showLoadingIcon();
     let selectedDrink = item.name;
     selectedDrink = selectedDrink.replace(/\s+/g, '_');
     let drinkUrl =
@@ -74,7 +85,6 @@ let cocktailRepository = (function () {
         return response.json();
       })
       .then(function (details) {
-        hideLoadingIcon();
         item.glass = details.strGlass;
         console.log('--' + details.strGlass); //Why undefined?
         item.instructions = details.strInstructions;
@@ -83,34 +93,22 @@ let cocktailRepository = (function () {
         // item.measure = details.strMeasure1; add for-loop to iterate thru object?
       })
       .catch(function (e) {
-        hideLoadingIcon();
         console.error(e);
       });
   }
 
-  var spinner = document.querySelector('.loader');
-  function showLoadingIcon() {
-    console.log('LOADING');
-
-    spinner.classList.add('spin');
-    setTimeout(function () {}, 1500);
-  }
-
-  function hideLoadingIcon() {
-    spinner.classList.remove('spin');
-    console.log('DONE');
-  }
-
   return {
     getAll: getAll,
-    loadList: loadList,
-    addListItem: addListItem,
+    loadCategoryList: loadCategoryList,
+    addCategoryListItem: addCategoryListItem,
+    addDrinkListItem: addDrinkListItem,
   };
 })();
 
-cocktailRepository.loadList().then(function () {
+cocktailRepository.loadCategoryList().then(function () {
   // Now the data is loaded!
+  console.log('Data loaded! now getAll()');
   cocktailRepository.getAll().forEach(function (category) {
-    cocktailRepository.addListItem(category);
+    cocktailRepository.addCategoryListItem(category);
   });
 });
