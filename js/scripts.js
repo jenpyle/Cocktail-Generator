754efe98c224d7dd43dbac7ac7d206ce8f7a6aad
jenpyle <jennypyle440@gmail.com>
2021-02-15 15:35:42 -0700

    Clean up code, update README.md

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,6 @@ let cocktailRepository = (function () {
         json.drinks.forEach(function (item) {
           // this anonymous function is the callback of forEach function, item is the parameter, item is the element of the json.drinks array which is an object
           add(item.strCategory);
-          console.log('--- ' + cocktailCategories);
         });
       })
       .catch(function (error) {
@@ -27,6 +26,60 @@ let cocktailRepository = (function () {
 
   function getAll() {
     return cocktailCategories;
+  }
+
+  function addDrinkDetails(response) {
+    let selectedDrink = response[0];
+
+    let drinkDetailsElem = document.querySelector('.cocktail-details');
+    let drinkTitleElem = document.createElement('h3');
+
+    drinkDetailsElem.innerHTML = '';
+    drinkTitleElem.innerText = selectedDrink.strDrink;
+    drinkDetailsElem.appendChild(drinkTitleElem);
+
+    let drinkInstructionsElem = document.createElement('p');
+    drinkInstructionsElem.innerText = selectedDrink.strInstructions;
+    drinkDetailsElem.appendChild(drinkInstructionsElem);
+
+    let drinkImageElem = document.createElement('img');
+    drinkImageElem.src = selectedDrink.strDrinkThumb;
+    drinkDetailsElem.appendChild(drinkImageElem);
+  }
+
+  function fetchDetailsByDrinkName(name) {
+    console.log('ooo' + name);
+    return fetch(
+      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`
+    )
+      .then((response) => response.json())
+      .then((json) => json.drinks);
+  }
+
+  function addDrinkListItem(drink) {
+    let drinkItem = document.createElement('li');
+    let drinkButton = document.createElement('button');
+    let drinksButtons = document.querySelector('.cocktail-drinks__buttons');
+
+    drinkButton.innerText = drink.strDrink;
+    drinkItem.appendChild(drinkButton);
+    drinksButtons.appendChild(drinkItem);
+
+    drinkButton.addEventListener('click', function () {
+      let drinkName = drink.strDrink;
+      fetchDetailsByDrinkName(drinkName).then((response) => {
+        addDrinkDetails(response);
+      });
+    });
+  }
+
+  function fetchDrinksByCategory(category) {
+    console.log('HERE');
+    return fetch(
+      `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`
+    )
+      .then((response) => response.json()) //what do these do?
+      .then((json) => json.drinks);
   }
 
   function addCategoryListItem(category) {
@@ -43,90 +96,15 @@ let cocktailRepository = (function () {
           addDrinkListItem(drink);
         });
       });
-    }); //Event Listener
+    });
     categoryListItem.appendChild(categoryButton); //append button to list item
     element.appendChild(categoryListItem); //append list item to parent
-  }
-
-  function fetchDrinksByCategory(category) {
-    console.log('HERE');
-    return fetch(
-      `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`
-    )
-      .then((response) => response.json()) //what do these do?
-      .then((json) => json.drinks);
-  }
-
-  function addDrinkListItem(drink) {
-    let drinkItem = document.createElement('li');
-    let drinkButton = document.createElement('button');
-    let drinksButtons = document.querySelector('.cocktail-drinks__buttons');
-
-    drinkButton.innerText = drink.strDrink;
-    drinkItem.appendChild(drinkButton);
-    drinksButtons.appendChild(drinkItem);
-
-    drinkButton.addEventListener('click', function () {
-      let drinkName = drink.strDrink;
-      fetchDetailsByDrinkName(drinkName)
-        .then(response => {
-          let selectedDrink = response[0];
-
-          let drinkDetailsElem = document.querySelector('.cocktail-details');
-          let drinkTitleElem = document.createElement('h3');
-
-          drinkDetailsElem.innerHTML = '';
-          drinkTitleElem.innerText = selectedDrink.strDrink;
-          drinkDetailsElem.appendChild(drinkTitleElem);
-
-          let drinkInstructionsElem = document.createElement('p');
-          drinkInstructionsElem.innerText = selectedDrink.strInstructions;
-          drinkDetailsElem.appendChild(drinkInstructionsElem);
-
-          let drinkImageElem = document.createElement('img');
-          drinkImageElem.src = selectedDrink.strDrinkThumb;
-          drinkDetailsElem.appendChild(drinkImageElem);
-        });
-    });
-  }
-
-  function fetchDetailsByDrinkName(name) {
-    console.log('ooo' + name);
-    return fetch(
-      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`
-    )
-      .then((response) => response.json())
-      .then((json) => json.drinks);
-  }
-
-  function loadMoreDetails(item) {
-    let selectedDrink = item.name;
-    selectedDrink = selectedDrink.replace(/\s+/g, '_');
-    let drinkUrl =
-      'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' +
-      selectedDrink;
-    return fetch(drinkUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (details) {
-        item.glass = details.strGlass;
-        console.log('--' + details.strGlass); //Why undefined?
-        item.instructions = details.strInstructions;
-        // item.ingredient = details.strIngredient1; add for-loop to iterate thru object?
-        item.isAlcoholic = details.strAlcoholic;
-        // item.measure = details.strMeasure1; add for-loop to iterate thru object?
-      })
-      .catch(function (e) {
-        console.error(e);
-      });
   }
 
   return {
     getAll: getAll,
     loadCategoryList: loadCategoryList,
     addCategoryListItem: addCategoryListItem,
-    addDrinkListItem: addDrinkListItem,
   };
 })();
 
