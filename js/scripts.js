3e4a63f5b11b24724f0fec8903f4ef2750793c70
jenpyle <jennypyle440@gmail.com>
2021-02-25 12:46:46 -0700

    fix names of classes, functions, and variables to align with generic filter naming

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,17 @@
+//Ask about order of functions
+//what is the naming convention? is it - or camelcase?
+
 let cocktailRepository = (function () {
-  let cocktailCategories = [];
-  let apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+  let filteredOptionsList = [];
 
   function add(listItem) {
-    cocktailCategories.push(listItem);
+    if (listItem === '') {
+      return;
+    }
+    filteredOptionsList.push(listItem);
   }
-
-  // function loadCategoryList() {
-  //   return fetch(apiUrl) // fetch returns a Promise
-  //     .then(function (response) {
-  //       //same thing as last 2 lines of fetchDrinksByCategory function
-  //       return response.json();
-  //     })
-  //     .then(function (json) {
-  //       json.drinks.forEach(function (item) {
-  //         // this anonymous function is the callback of forEach function, item is the parameter, item is the element of the json.drinks array which is an object
-  //         if (item.strCategory === '') {
-  //           return;
-  //         }
-  //         add(item.strCategory);
-  //       });
-  //     })
-  //     .catch(function (error) {
-  //       console.error(error);
-  //     });
-  // }
-
-  function loadFilteredList(filterLetter, strOption) {
+  //is there a way to make this function short like the other fetching functions?
+  function fetchFilteredList(filterLetter, strOption) {
     return fetch(
       `https://www.thecocktaildb.com/api/json/v1/1/list.php?${filterLetter}=list`
     )
@@ -37,7 +22,9 @@ let cocktailRepository = (function () {
         json.drinks.forEach(function (item) {
           //Why doesn't item.strOption work?
           // add(item.strOption);
-          add(item.strIngredient1);
+          // add(item.strIngredient1);
+          // add(item.strGlass);
+          add(item.strAlcoholic);
         });
       })
       .catch(function (error) {
@@ -46,13 +33,13 @@ let cocktailRepository = (function () {
       .then(function () {
         //What is going on here? Are these parameters correct?
         getAll().forEach(function (itemValue) {
-          addCategoryListItem(filterLetter, itemValue);
+          addFilteredListItem(filterLetter, itemValue);
         });
       });
   }
 
   function getAll() {
-    return cocktailCategories;
+    return filteredOptionsList;
   }
 
   function fetchDetailsByDrinkName(name) {
@@ -68,7 +55,7 @@ let cocktailRepository = (function () {
     drinkItem.classList.add('group-list-item');
     let drinkButton = document.createElement('button');
     drinkButton.classList.add('btn');
-    let drinksButtons = document.querySelector('.cocktail-drinks');
+    let drinksButtons = document.querySelector('.drinks');
 
     drinkButton.setAttribute('data-toggle', 'modal');
     drinkButton.setAttribute('data-target', '#modal-container');
@@ -93,32 +80,26 @@ let cocktailRepository = (function () {
     )
       .then((response) => response.json())
       .then((json) => json.drinks);
-    // .catch(function (error) {
-    //   console.error(error);
-    // })
   }
 
-  function addCategoryListItem(filterLetter, itemValue) {
-    let element = document.querySelector('.cocktail-categories');
-    let categoryListItem = document.createElement('li');
-    categoryListItem.classList.add('group-list-item');
-    let categoryButton = document.createElement('button');
-    categoryButton.classList.add('btn');
-    categoryButton.innerText = itemValue;
+  function addFilteredListItem(filterLetter, itemValue) {
+    let element = document.querySelector('.filtered-options');
+    let filteredListItem = document.createElement('li');
+    filteredListItem.classList.add('group-list-item');
+    let itemButton = document.createElement('button');
+    itemButton.classList.add('btn');
+    itemButton.innerText = itemValue;
 
-    categoryButton.addEventListener('click', function () {
+    itemButton.addEventListener('click', function () {
       fetchDrinksByFilter(filterLetter, itemValue).then((drinks) => {
-        console.log('0HERE----------' + drinks);
-        document.querySelector('.cocktail-drinks').innerHTML = ''; // clear all the drinks, otherwise it would keep appending the drinks of the clicked category at the end
+        document.querySelector('.drinks').innerHTML = ''; // clear all the drinks, otherwise it would keep appending the drinks of the clicked category at the end
         drinks.forEach((drink) => {
-          console.log('HERE-------' + drink);
-          // you can inspect api response under Network tab > XHR > Response
           addDrinkListItem(drink);
         });
       });
     });
-    categoryListItem.appendChild(categoryButton); //append button to list item
-    element.appendChild(categoryListItem); //append list item to parent
+    filteredListItem.appendChild(itemButton);
+    element.appendChild(filteredListItem);
   }
 
   function getIngredients(selectedDrink) {
@@ -195,10 +176,7 @@ let cocktailRepository = (function () {
   }
 
   return {
-    getAll: getAll,
-    // loadCategoryList: loadCategoryList,
-    loadFilteredList: loadFilteredList,
-    addCategoryListItem: addCategoryListItem,
+    fetchFilteredList: fetchFilteredList,
     toggleHideShow: toggleHideShow,
   };
 })();
